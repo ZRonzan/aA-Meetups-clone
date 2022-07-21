067887139ddca653b915381f32e6739a5597dc3a
ZRonzan <zeus.ronzan@gmail.com>
2022-07-21 10:37:10 -0700

    front end auth me phas 0 complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 import configureStore from "./store"
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 function Root() {
