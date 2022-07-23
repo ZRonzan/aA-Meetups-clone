d774d09918cd4b4c7e9fad3c5c54fc5cdf7d7bb3
ZRonzan <zeus.ronzan@gmail.com>
2022-07-22 18:30:00 -0700

    updated scripts for frontend and heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
 
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
@@ -34,7 +35,14 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-router.use('/api', apiRouter);
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
+
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
