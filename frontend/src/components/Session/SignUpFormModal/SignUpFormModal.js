e1f2ccd0b5c6849a69e3f14e761dc9cbd16fe672
ZRonzan <zeus.ronzan@gmail.com>
2022-07-31 16:08:17 -0700

    added required to sign up form field inputs

--- frontend/src/components/Session/SignUpFormModal/SignUpFormModal.js
+++ frontend/src/components/Session/SignUpFormModal/SignUpFormModal.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom"
 import { userSignUp } from "../../../store/session"
 import "./UserSignUpFormModal.css"
 
-export default function UserSignUpPage({setShowModal}) {
+export default function UserSignUpPage({ setShowModal }) {
     const dispatch = useDispatch()
     const history = useHistory()
 
@@ -43,95 +43,100 @@ export default function UserSignUpPage({setShowModal}) {
 
     useEffect(() => {
         setResponse("")
-    },[firstName,lastName,email,password,passwordConfirmation])
+    }, [firstName, lastName, email, password, passwordConfirmation])
 
     return (
         <>
-        <div className="signup-form-container">
-            <form
-                onSubmit={handleSubmit}
-                className="signup-form"
-            >
-                <div className="signup-form-cross signup" onClick={() => setShowModal(false)}>
-                    <i className="fa-solid fa-xmark"></i>
-                </div>
-                <div className="signup-form">
-                    <h2>Sign up</h2>
-                </div>
-                <label className="signup-form">First Name:
-                    <input
-                        className="signup-form"
-                        onChange={e => setFirstName(e.target.value)}
-                        value={firstName}
-                    >
-                    </input>
-                </label>
-                <label className="signup-form">Last Name:
-                    <input
-                        className="signup-form"
-                        onChange={e => setLastName(e.target.value)}
-                        value={lastName}
-                    >
-                    </input>
-                </label>
-                <label className="signup-form">Email:
-                    <input
-                        className="signup-form"
-                        onChange={e => setEmail(e.target.value)}
-                        value={email}
-                    >
-                    </input>
-                </label>
-                <label className="signup-form">Password:
-                    <input
-                        className="signup-form"
-                        type="password"
-                        onChange={e => setPassword(e.target.value)}
-                        value={password}
-                    >
-                    </input>
-                </label>
-                <label className="signup-form">Password Confirmation:
-                    <input
-                        className="signup-form"
-                        type="password"
-                        onChange={e => setPasswordConfirmation(e.target.value)}
-                        value={passwordConfirmation}
-                    >
-                    </input>
-                </label>
-                <div>
-                    {!(password === passwordConfirmation) && (
-                        <text>Passwords do not match</text>
+            <div className="signup-form-container">
+                <form
+                    onSubmit={handleSubmit}
+                    className="signup-form"
+                >
+                    <div className="signup-form-cross signup" onClick={() => setShowModal(false)}>
+                        <i className="fa-solid fa-xmark"></i>
+                    </div>
+                    <div className="signup-form">
+                        <h2>Sign up</h2>
+                    </div>
+                    <label className="signup-form">First Name:
+                        <input
+                            required
+                            className="signup-form"
+                            onChange={e => setFirstName(e.target.value)}
+                            value={firstName}
+                        >
+                        </input>
+                    </label>
+                    <label className="signup-form">Last Name:
+                        <input
+                            required
+                            className="signup-form"
+                            onChange={e => setLastName(e.target.value)}
+                            value={lastName}
+                        >
+                        </input>
+                    </label>
+                    <label className="signup-form">Email:
+                        <input
+                            required
+                            className="signup-form"
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
+                        >
+                        </input>
+                    </label>
+                    <label className="signup-form">Password:
+                        <input
+                            required
+                            className="signup-form"
+                            type="password"
+                            onChange={e => setPassword(e.target.value)}
+                            value={password}
+                        >
+                        </input>
+                    </label>
+                    <label className="signup-form">Password Confirmation:
+                        <input
+                            required
+                            className="signup-form"
+                            type="password"
+                            onChange={e => setPasswordConfirmation(e.target.value)}
+                            value={passwordConfirmation}
+                        >
+                        </input>
+                    </label>
+                    <div>
+                        {!(password === passwordConfirmation) && (
+                            <text>Passwords do not match</text>
+                        )}
+                    </div>
+                    <button disabled={password !== passwordConfirmation} className="signup-form">Sign Up</button>
+                    {response && (
+                        <>
+                            {response.message === "Validation error" && (
+                                <>
+                                    <ul className="signup-form">
+                                        <h3>Sign up validation errors:</h3>
+                                        {response.errors && (
+                                            response.errors.map((message, i) => {
+                                                return (<li key={i}>{Object.values(message)}</li>)
+                                            })
+                                        )}
+                                    </ul>
+                                </>
+                            )}
+                            {response.message === "User already exists" && (
+                                <>
+                                    <h4>Sign up email error:</h4>
+                                    <ul className="signup-form">
+                                        <li>{Object.values(response.errors.email)}</li>
+                                    </ul>
+                                </>
+                            )}
+                        </>
                     )}
-                </div>
-                <button disabled={password !== passwordConfirmation} className="signup-form">Sign Up</button>
-                {response && (
-                    <>
-                        {response.message === "Validation error" && (
-                            <>
-                                <ul className="signup-form">
-                                    <h3>Sign up validation errors:</h3>
-                                    {response.errors && (
-                                        response.errors.map((message, i) => {
-                                            return (<li key={i}>{Object.values(message)}</li>)
-                                        })
-                                    )}
-                                </ul>
-                            </>
-                        )}
-                        {response.message === "User already exists" && (
-                            <>
-                                <h4>Sign up email error:</h4>
-                                <ul className="signup-form">
-                                    <li>{Object.values(response.errors.email)}</li>
-                                </ul>
-                            </>
-                        )}
-                    </>
-                )}
-            </form>
-        </div>
+                </form>
+            </div>
         </>
     )
 }
